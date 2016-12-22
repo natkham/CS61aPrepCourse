a8fe2d524317d7c384ff30d51cb5666a066c070e
Natalie <Nat@Natalies-MBP-2.attlocal.net>
2016-12-22 11:17:08 -0800

    Updated AFTtT for Chapter 2

--- Chap2/AFTtT.rb
+++ Chap2/AFTtT.rb
@@ -1,8 +1,11 @@
-###Hours in a year
+puts "Hours in a year:"
 puts 24 * 365
 
-###Minutes in a decade
-puts 60 * ((365 * 8) + (366 * 2)) * 24
+puts "Minutes in a decade (not taking leap years into consideration):"
+puts ((365 * 10) * 24) * 60
 
-###Age in seconds
-puts (((18 / 4) * 366) + ((18 - (18 / 4)) * 365)) * (24 * 60 * 60)
+puts "My age in seconds (as of today):"
+puts (((365 * 18) + 4) + ((30 * 5) + 2) + 15) * 24 * 60 * 60
+
+puts "Author's age (estimated in years):"
+puts (((((1160 * 1000000) / 60) / 60 ) / 24) / 365)
