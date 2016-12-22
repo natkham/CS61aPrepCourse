9d86c61239a9a79c32a4347d5dadd47632a194ee
Natalie <Nat@Natalies-MBP-2.attlocal.net>
2016-12-22 11:26:02 -0800

    Cleaned up

--- chap2.rb
+++
@@ -1,8 +0,0 @@
-###Hours in a year
-puts 24 * 365
-
-###Minutes in a decade
-puts 60 * ((365 * 8) + (366 * 2)) * 24
-
-###Age in seconds
-puts (((18 / 4) * 366) + ((18 - (18 / 4)) * 365)) * (24 * 60 * 60)
