b97114e83e32b1dcc5faf692f861fa4452ddb7fa
Natalie <Nat@Natalies-MBP-2.attlocal.net>
2016-12-23 10:30:02 -0800

    Chapter 5 added

---
+++ Chap5/chap5.rb
@@ -0,0 +1,18 @@
+###5.1
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts ''
+
+###5.3
+puts gets
+
+puts ''
+
+###5.5
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
