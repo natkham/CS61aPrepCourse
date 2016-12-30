f6898137cb1785f333c4a2bff6676db9569e27d8
Natalie <Nat@Natalies-MBP-2.attlocal.net>
2016-12-30 00:10:31 -0800

    Deaf Grandma Extended

--- Chap7/deafGrandmaWithExtension.rb
+++ Chap7/deafGrandmaWithExtension.rb
@@ -1,16 +1,24 @@
 puts 'Talk to your Grandma.'
 input = ''
+bye = 0
 while true
 	input = gets.chomp
 	if input == 'BYE'
-		break
+		if bye == 2
+			break
+		elsif bye < 2
+			bye = bye + 1
+			puts 'SORRY I COULDN\'T HEAR YOU. WHAT?'
+		end
 	elsif input != input.upcase
 		puts 'HUH?! SPEAK UP, SONNY!'
+		bye = 0
 	elsif input == input.upcase
 		year = 0
 		while !((year > 1929) && (year < 1951))
 			year = rand(1951)
 		end
 		puts 'NO, NOT SINCE ' + year.to_s + '!'
+		bye = 0
 	end
 end
