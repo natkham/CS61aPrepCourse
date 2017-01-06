4dd99e08532a7f218c4f8afbd3f978152a9721a0
Natalie <Nat@Natalies-MBP-2.attlocal.net>
2017-01-05 20:20:20 -0800

    99 Bottles of Beer - revised

--- Chap7/99Bottles.rb
+++ Chap7/99Bottles.rb
@@ -1,5 +1,5 @@
 beer = 99
-while beer > -1
+while beer > 0
 	if beer == 1
 		puts beer.to_s + ' bottle of beer on the wall,'
 		puts beer.to_s + ' bottle of beer.'
@@ -13,5 +13,12 @@ while beer > -1
 		puts beer.to_s + ' bottle of beer on the wall.'
 	elsif beer == 0
 		puts 'No more bottles of beer on the wall.'
+	elsif beer > 1
+		puts beer.to_s + ' bottles of beer on the wall.'
 	end
 end
+
+puts 'No more bottles of beer on the wall,'
+puts 'No more bottles of beer.'
+puts 'Go to the store and buy some more,'
+puts '99 bottles of beer on the wall.'
