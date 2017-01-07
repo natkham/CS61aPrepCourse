19bbae1300833a4f6d229d444dfbece44256f648
Natalie <Nat@Natalies-MBP-2.attlocal.net>
2017-01-06 21:59:35 -0800

    Build Your Own Playlist

---
+++ Chap11/buildYourOwnPlaylist.rb
@@ -0,0 +1,30 @@
+def shuffle array
+	rec_shuf array, []
+end
+
+def rec_shuf unshuffled, shuffled
+	if unshuffled.length == 0
+		return shuffled
+	end
+
+	chosen = unshuffled[rand(unshuffled.length)]
+	still_unshuffled = []
+
+	unshuffled.each do |word|
+		if word != chosen
+			still_unshuffled.push word
+		end
+	end
+	shuffled.push chosen
+	rec_shuf still_unshuffled, shuffled
+end
+
+newPlaylist = shuffle(Dir['**/*.mp3'])
+
+File.open 'playlist.m3u', 'w' do |f|
+	newPlaylist.each do |mp3|
+		f.write mp3 + "\n"
+	end
+end
+
+puts 'Done!'
